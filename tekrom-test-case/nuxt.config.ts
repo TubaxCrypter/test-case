3cf9d41154f77de97b1e52617019974f4dbe6a27
Mert Dirilik <mertdirilik@gmail.com>
2022-12-21 12:59:26 +0300

    tailwindcss & pinia install
    tailwind3
    pinia store
    kurulumları

--- tekrom-test-case/nuxt.config.ts
+++ tekrom-test-case/nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
-})
+export default {
+    buildModules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+    ],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        ['@pinia/nuxt', { disableVuex: false }],
+
+    ],
+    build: {
+        postcss: {
+            plugins: {
+                tailwindcss: {},
+                autoprefixer: {},
+            },
+        },
+    },
+    css: [
+        '@/assets/css/main.css',
+    ],
+
+
+}
