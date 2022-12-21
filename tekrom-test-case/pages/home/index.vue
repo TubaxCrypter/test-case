864c8cc269724ca620d0272b0f491ea3ac4d2881
Mert Dirilik <mertdirilik@gmail.com>
2022-12-21 14:07:14 +0300

    Ürün Düzenleme

--- tekrom-test-case/pages/home/index.vue
+++ tekrom-test-case/pages/home/index.vue
@@ -28,25 +28,35 @@
         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="limit" type="number" placeholder="Ürün Sayısı">
       </div>
     </div>
-    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div class="p-10 grid grid-cols-4 gap-4">
       <div class="max-w-sm rounded overflow-hidden shadow-lg" v-for="product in productData" :key="product.id">
         <img class="w-full" :src="product.images[0]" alt="Sunset in the mountains">
         <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2"> {{product.title}}
+          <div v-if="!editMode" class="font-bold text-xl mb-2"> {{product.title}}
           </div>
-          <p class="text-gray-700 text-base">
+          <textarea v-if="editMode" v-model="product.title" class="font-bold border-rose-200 border-4 border-indigo-500/100  text-xl mb-2"> {{product.title}}
+          </textarea>
+          <p  v-if="!editMode" class="text-gray-700 text-base">
             {{product.description}}
           </p>
+          <textarea v-if="editMode" v-model="product.description"  class="font-bold border-rose-200 border-4 border-indigo-500/100  text-xl mb-2"> {{product.description}}
+          </textarea>
           <span class="text-sm font-semibold">Price</span>
-          &nbsp;<span class="font-bold text-xl">{{product.price}}</span>&nbsp;
+          &nbsp;
+          <span  v-if="!editMode" class="font-bold text-xl">{{product.price}}</span>
+          <textarea v-model="product.price" v-if="editMode" class="font-bold border-rose-200 text-xl">{{product.price}}</textarea>
+          &nbsp;
           <span class="text-sm font-semibold">$</span>
         </div>
         <div class="px-6 pt-4 pb-2 mb-2">
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#:{{product.id}}</span>
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{{product.category.name}}</span>
           <div class="grid grid-cols-3 gap-2 justify-end align-middle">
-            <button class="bg-orange-500 block mx-auto text-white text-sm uppercase rounded shadow-md px-6 py-2 mt-2" >
+            <button  v-if="!editMode"  class="bg-orange-500 block mx-auto text-white text-sm uppercase rounded shadow-md px-6 py-2 mt-2" @click="editMode = true">
               Düzenle
+            </button>
+            <button  v-if="editMode"  class="bg-orange-500 block mx-auto text-white text-sm uppercase rounded shadow-md px-6 py-2 mt-2" @click="editMode = false">
+              Kaydet
             </button>
             <button class="bg-red-600 block mx-auto text-white text-sm uppercase rounded shadow-md px-6 py-2 mt-2" @click="removeProduct(product.id)" >
               Sil
@@ -83,6 +93,7 @@ export default ({
       productData:[],
       offset:"0",
       limit:"10",
+      editMode:false,
       user:{
         email:"",
         password:"",
